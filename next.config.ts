8d4dc01a3a00f1e377b707cd801568c75027e902
XKrulesF3lipe <vitorhugo.noronha15@gmail.com>
2025-12-09 12:55:34 -0300

    Configurando exportação corretamente

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  images:{
-    unoptimized: true,
-  }
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',               // Gera HTML estático (Obrigatório para GitHub Pages)
+  basePath: '/Inatel2',           // Define a pasta base (Obrigatório pois seu site não está na raiz do domínio)
+  images: {
+    unoptimized: true,            // Necessário para imagens funcionarem no modo estático
+  },
 };
 
 export default nextConfig;
